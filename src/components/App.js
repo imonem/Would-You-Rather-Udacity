ec8c7ecb28def24e2d8837960752dc75fd7e6439
Amr Nabeel <amr.nabeel@gmail.com>
2021-05-27 05:40:27 +0200

    Base Framework - no styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,13 @@ function App() {
   return (
     <div className='App'>
       <h1>Login page</h1>
+      <h1>Register New User</h1>
+      <h1>Loading Bar</h1>
+      <h1>NAV</h1>
+      <h1>Dashboard</h1>
+      <h1>New Question</h1>
+      <h1>Leader Board</h1>
+      <h1>Footer</h1>
     </div>
   );
 }
