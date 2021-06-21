f5ccebd2d9fc96b31bf8544056c745ee02c279f4
Amr Nabeel <amr.nabeel@gmail.com>
2021-06-21 04:23:43 +0200

    Enabled redux devtool with middleware and enhancers

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 //Store exist here, video #2 Lesson 7 concept 7
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import reducer from "./reducers";
-import middleware from "./middleware";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import composeEnhancers from './middleware';
+import { BrowserRouter } from 'react-router-dom';
 
-const store = createStore(reducer, middleware); //middleware logger is added in video #3 lesson 7 concept 7
+const store = createStore(reducer, composeEnhancers); //middleware logger is added in video #3 lesson 7 concept 7 || composeEnhancers to enable redux devtool
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById('root')
 );
